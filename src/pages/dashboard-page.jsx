bfa50a0b9fac202a7ddc6c1cf3d29424efd981eb
1appiah <92669349+1appiah@users.noreply.github.com>
2025-04-19 18:34:05 +0000

    dashboard fixed and will show user link when logged in or login button when user is not logged in

--- src/pages/dashboard-page.jsx
+++ src/pages/dashboard-page.jsx
@@ -1,14 +1,42 @@
 import Heading from "@/components/heading";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
-import { useState } from "react";
+import { useState,useLayoutEffect } from "react";
 import { copyLink } from "@/lib/utils";
 import CopyLinkButton from "@/components/copy-link-button";
 import { toast } from "sonner";
 import { Toaster } from "@/components/ui/sonner";
+import axiosInstance from "../axiosInstance";
+import { Link } from "react-router-dom";
+import {Button} from "@/components/ui/button"
+
+///
+// this dashboard should show a users link when  logged in or show a login link to users who have not signed in
+/// so we will check local storage for access token it it exist it means the user has logged in
+
 
 export default function DashboardPage() {
   const [link, setLink] = useState("http://localhost:5173/send-image/1/");
  // <p className="">{`https://anonymous-image-react.onrender.com/send-image/${parseInt(ids)}/`}</p>
+
+ const [ids, setId] = useState("no")
+    let locals = localStorage.getItem('access_token')
+    const getId = ()=>{
+        axiosInstance.get(`api/imaging/dashboard/`,{
+            headers:{
+        Authorization: 'JWT ' + localStorage.getItem('access_token')
+    
+    },
+        }
+        )
+        .then((res) =>{
+            setId(res.data.id)
+        })
+        .catch(e=>{
+            console.log('login',e);
+            console.log("something is wrong")
+            
+        })
+    }
 
   const handleCopyToClipboard = async () => {
     const success = await copyLink(link);
@@ -19,6 +47,13 @@ export default function DashboardPage() {
       : toast("Copying link failed. Try again later");
   };
 
+  useLayoutEffect(()=>{
+    if (locals) {
+        getId();
+    }
+    
+    }, [])
+
   return (
     <div className="min-h-screen">
       <MaxWidthWrapper>
@@ -26,7 +61,10 @@ export default function DashboardPage() {
           Welcome to Anonymous Image Sharing
         </Heading>
 
-        <div className="mt-4 sm:mt-6 md:mt-8 p-6 sm:p-10 md:p-12 lg:p-16 xl:p-20 bg-blue-100 rounded-lg text-center mx-4 sm:mx-0">
+        {
+          ids !== "no" ? 
+          <>
+          <div className="mt-4 sm:mt-6 md:mt-8 p-6 sm:p-10 md:p-12 lg:p-16 xl:p-20 bg-blue-100 rounded-lg text-center mx-4 sm:mx-0">
           <p className="text-base sm:text-lg md:text-xl font-semibold text-blue-700 mb-4 sm:mb-6">
             Copy and share the link below to friends to receive images
             anonymously
@@ -46,6 +84,25 @@ export default function DashboardPage() {
           </div>
         </div>
         <Toaster />
+        </>
+        :
+        <>
+        <div className="mt-4 sm:mt-6 md:mt-8 p-6 sm:p-10 md:p-12 lg:p-16 xl:p-20 bg-blue-100 rounded-lg text-center mx-4 sm:mx-0">
+          <p className="text-base sm:text-lg md:text-xl font-semibold text-blue-700 mb-4 sm:mb-6">
+            Sign in or Register to receive image anonymously from friends
+          </p>
+
+          <div className="flex flex-col sm:flex-row justify-center items-center gap-3 sm:gap-4 py-3 sm:py-4 bg-white rounded-md px-4 sm:px-6 w-full max-w-2xl mx-auto">
+              <Link to={'/log-in'}>
+                 <Button className="bg-blue-100 text-blue-800 hover:bg-blue-200">Log in</Button>
+              </Link>
+
+          </div>
+        </div>
+        
+        </>
+        }
+
       </MaxWidthWrapper>
     </div>
   );
