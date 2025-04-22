c09d59ad447f64722e1f225da96f024a423bd63e
Yaw Obeng Asante <yawobengasante@gmail.com>
2025-04-22 23:28:51 +0000

    Chore: minor changes to image page loader

--- src/components/images-page-loader.jsx
+++ src/components/images-page-loader.jsx
@@ -11,6 +11,7 @@ export default function ImagesPageLoader() {
                 <SkeletonCard/>
                 <SkeletonCard/>
                 <SkeletonCard/>
+                <SkeletonCard/>
             </div>
         </MaxWidthWrapper>
     </div>
